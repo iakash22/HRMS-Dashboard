1de4edc3dc650f8b764fe4fc81ca1fda04d18a35
Akash kumar <94979388+iakash22@users.noreply.github.com>
2025-04-12 15:01:52 +0530

    change icon stroke and index

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,32 +1,41 @@
 import SearchIcon from './icons/search.svg?react';
 import DropdownIcon from './icons/dropDown.svg?react';
-// import { ReactComponent as AttendanceIcon } from './icons/attandance.svg';
-// import { ReactComponent as CheckboxIcon } from './icons/checkbox-active.svg';
-// import { ReactComponent as ChevronDownIcon } from './icons/chevron-down.svg';
-// import { ReactComponent as DocumentIcon } from './icons/document.svg';
-// import { ReactComponent as MoreIcon } from './icons/more.svg';
-// import { ReactComponent as UploadIcon } from './icons/upload.svg';
-// import { ReactComponent as UserIcon } from './icons/users.svg';
-// import { ReactComponent as CheckCircleIcon } from './icons/check-circle.svg';
-// import { ReactComponent as NotificationIcon } from './icons/notifications.svg';
-// import { ReactComponent as EyeHiddenIcon } from './icons/eye-hidden.svg';
-// import { ReactComponent as EyeOpenIcon } from './icons/eye-open.svg';
-// import { ReactComponent as UserAddIcon } from './icons/user-add.svg';
+import ChevronDownIcon from './icons/chevron-down.svg?react';
+import AttendanceIcon from './icons/attandance.svg?react';
+import CheckboxActiveIcon from './icons/checkbox-active.svg?react';
+import CheckboxInActiveIcon from './icons/checkbox-inactive.svg?react';
+import DocumentIcon from './icons/document.svg?react';
+import MoreIcon from './icons/more.svg?react';
+import UploadIcon from './icons/upload.svg?react';
+import UserIcon from './icons/users.svg?react';
+import UserAddIcon from './icons/user-add.svg?react';
+import CheckCircleIcon from './icons/check-circle.svg?react';
+import NotificationIcon from './icons/notifications.svg?react';
+import EyeHiddenIcon from './icons/eye-hidden.svg?react';
+import EyeOpenIcon from './icons/eye-open.svg?react';
+import CalendarIcon from './icons/calendar.svg?react';
+import LeaveIcon from './icons/leave.svg?react';
+import LogoutIcon from './icons/logout.svg?react';
+import MailIcon from './icons/mail.svg?react';
 
 export {
     SearchIcon,
     DropdownIcon,
-    // AttendanceIcon,
-    // CheckboxIcon,
-    // ChevronDownIcon,
-    // DocumentIcon,
-    // MoreIcon,
-    // UploadIcon,
-    // UserIcon,
-    // CheckCircleIcon,
-    // NotificationIcon,
-    // EyeHiddenIcon,
-    // EyeOpenIcon,
-    // UserAddIcon
-    
+    ChevronDownIcon,
+    AttendanceIcon,
+    CheckboxActiveIcon,
+    CheckboxInActiveIcon,
+    DocumentIcon,
+    MoreIcon,
+    UploadIcon,
+    UserIcon,
+    CheckCircleIcon,
+    NotificationIcon,
+    EyeHiddenIcon,
+    EyeOpenIcon,
+    UserAddIcon,
+    CalendarIcon,
+    LeaveIcon,
+    LogoutIcon,
+    MailIcon,
 };
